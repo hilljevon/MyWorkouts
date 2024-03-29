e339856603b6252d855b9d1fbc0343f3c743877f
Jevon Hill <jevon623@gmail.com>
2024-03-28 21:02:40 -0700

    Initial commit

---
+++ components/AuthButton.tsx
@@ -0,0 +1,38 @@
+// import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function AuthButton() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hey,<span className="text-blue-400 m-0 p-0">{user.email}</span>!
+      <form action={signOut}>
+        <button className="py-2 px-3 rounded-md no-underline text-white bg-slate-600 hover:bg-btn-background-hover">
+          Logout
+        </button>
+      </form>
+    </div>
+  ) : (
+    <Link
+      href="/login"
+      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+    >
+      Login
+    </Link>
+  );
+}
